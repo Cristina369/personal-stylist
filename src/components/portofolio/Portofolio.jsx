5d20434c3ab18b1dc36aae7f528715276c97e681
Cristina <cristy.wolf666@gmail.com>
2023-02-15 12:48:56 +0200

    upl

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -5,13 +5,14 @@ const Portofolio = () => {
   return (
     <>
         <section className='bg-white w-10/12 h-screen dark:bg-black absolute right-0'>
-            <div>
-                <h1>Behind the scenes</h1>
+            <div className=''>
+                <h1 className='text-secondary dark:text-primary text-9xl font-extrabold fontM absolute top-20 titleP left-[500px] pb-4 title dark:shadowNone'>Behind</h1>
+                <h1 className='text-secondary dark:text-primary text-9xl font-black fontM absolute top-[570px] right-[210px] flex flex-col title dark:shadowNone'><span className='text-7xl font-extrabold'>the</span>SCENES</h1>
             </div>
-            <div className='grid grid-cols-2'>
+            <div className=' bg-primary dark:bg-secondary w-full grid grid-cols-2 gap-1 justify-center justify-items-center p-24 pr-32'>
                 {portofolio.map((val, index)=>(
                     <div className='' key={index}>
-                        <img className='h-96' src={val.image} alt={val.title}/>
+                        <img className='w-[500px]' src={val.image} alt={val.title}/>
                     </div>
                 ))}
             </div>
--- src/data.js
+++ src/data.js
@@ -1,26 +1,31 @@
 export const portofolio = [
   {
     id: 1,
-    image: require('./images/photo_7.jpg'),
-    title: "Sofia Style",
-  },
-    {
-    id: 1,
-    image: require('./images/photo_8.jpg'),
-    title: "Sofia Style",
-  },
-    {
-    id: 1,
-    image: require('./images/photo_9.jpg'),
-    title: "Sofia Style",
-  },
-    {
-    id: 1,
     image: require('./images/photo_5.jpg'),
     title: "Sofia Style",
-  },  {
-    id: 1,
+  },
+    {
+    id: 2,
+    image: require('./images/photo_11.jpg'),
+    title: "Sofia Style",
+  },
+    {
+    id: 3,
+    image: require('./images/photo_10.jpg'),
+    title: "Sofia Style",
+  },
+    {
+    id: 4,
     image: require('./images/photo_6.jpg'),
+    title: "Sofia Style",
+  },  {
+    id: 5,
+    image: require('./images/photo_1.jpg'),
+    title: "Sofia Style",
+  },
+    {  
+    id: 5,
+    image: require('./images/photo_2.jpg'),
     title: "Sofia Style",
   },
 ]
